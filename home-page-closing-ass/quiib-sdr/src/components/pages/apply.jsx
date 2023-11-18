ae6338e93b461177064eb9d980450218f8738bcb
Drew Levine <50465206+drewlev@users.noreply.github.com>
2023-11-18 17:19:53 -0500

    Update apply.jsx

--- home-page-closing-ass/quiib-sdr/src/components/pages/apply.jsx
+++ home-page-closing-ass/quiib-sdr/src/components/pages/apply.jsx
@@ -1,5 +1,4 @@
 import Nav from "../nav"
-import ApplyForm from '../ApplyForm'
 import "./Landing.css"
 import  "./apply.css"
 
@@ -8,7 +7,6 @@ function Apply() {
     return (
       <div className='Landing apply'>
           <Nav/>
-          <ApplyForm/>
           <div className='light-box'>   
           </div>
       </div>
