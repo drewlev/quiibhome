26b882b34d8102f36fd6f11a6d69fbceb043aefe
drew-rally <drewlev@umich.edu>
2023-11-18 19:12:04 -0500

    learn is typeform

--- home-page-closing-ass/quiib-sdr/src/App.jsx
+++ home-page-closing-ass/quiib-sdr/src/App.jsx
@@ -2,13 +2,27 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './components/pages/Landing';
 import Apply from './components/pages/apply';
+import { useEffect } from 'react';
+
+
 
 function App() {
+  const RedirectToExternal = () => {
+    useEffect(() => {
+      window.location.href = 'https://m3jcdnom1s3.typeform.com/to/c0BpoK9m';
+    }, []);
+
+    return null; // or some loading indicator until the redirect happens
+  };
+
+
+
   return (
     <div>
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/apply" element={<Apply />} />
+      <Route path="/learn" element={<RedirectToExternal />} />
     </Routes>
     
     </div>
